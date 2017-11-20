8d006996497d9723b5306a8e2142a29b3fe5f0ef
leo <ivashchenko.leo@gmail.com>
2017-11-21 00:26:43 +0300

    add a little comment about configs

--- src/main/java/binance/config/IConfigLoader.java
+++ src/main/java/binance/config/IConfigLoader.java
@@ -3,6 +3,10 @@ package binance.config;
 /**
  * An interface for loading a config. Implementation depends on a config type.
  * For example, we can make the config in xml or properties and create a loader for it.
+ *
+ * I believe it would be better to put the config somewhere else, not in resources, so
+ * we could change some parameters and just restart the application. But it will be easier
+ * for you to execute this app for the test purpose with configs in resources.
  */
 public interface IConfigLoader {
     /**
